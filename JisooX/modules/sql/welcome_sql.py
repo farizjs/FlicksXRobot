5f6d190513ecfbe51dac83e5ba02f4cbf77fc9ae
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-09-28 20:47:43 +0700

    Update welcome_sql.py

--- JisooX/modules/sql/welcome_sql.py
+++ JisooX/modules/sql/welcome_sql.py
@@ -5,8 +5,8 @@ from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 from JisooX.modules.helper_funcs.msg_types import Types
 from JisooX.modules.sql import SESSION, BASE
 
-DEFAULT_WELCOME = "Hey {first}, Welcome to {chatname}! How are you?"
-DEFAULT_GOODBYE = "Nice knowing ya!"
+DEFAULT_WELCOME = "Hai {first}, Selamat datang di {chatname}! Semoga betah ya?"
+DEFAULT_GOODBYE = "Selamat tinggal kawan!"
 
 class Welcome(BASE):
     __tablename__ = "welcome_pref2"
