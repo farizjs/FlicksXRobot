5c5b508f256dc1f4aa8e410afe64ed7c39a6eb75
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-10-03 12:24:07 +0700

    Update __main__.py

--- JisooX/__main__.py
+++ JisooX/__main__.py
@@ -24,6 +24,47 @@ from JisooX.modules.helper_funcs.chat_status import is_user_admin
 from JisooX.modules.helper_funcs.misc import paginate_modules
 from JisooX.modules.connection import connected
 from JisooX.modules.connection import connect_button
+from pyrogram.types import (
+  Message, 
+)
+from pyrogram import filters, Client 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import requests
+import os
+import re
+
+API_ID = os.environ.get("API_ID", None) 
+API_HASH = os.environ.get("API_HASH", None) 
+TOKEN = os.environ.get("TOKEN", None) 
+BOT_ID = os.environ.get("BOT_ID", None)
+
+
+kuki = Client(
+      "KukiBot",
+      api_id=API_ID,
+      api_hash=API_HASH,
+      bot_token=TOKEN,
+)
+
+@kuki.on_message(
+    filters.text
+    & filters.reply
+    & ~filters.bot
+    & ~filters.edited,
+    group=2,
+)
+async def kukiai(client: Client, message: Message):
+  msg = message.text
+  chat_id = message.chat.id
+
+  Kuki =   requests.get(f"https://kuki-api.tk/api/botname/ownername/message={msg}").json()
+
+  Kuki =   requests.get(f"https://kuki-api.tk/api/message={msg}").json()
+
+  idz = f"{Kuki['reply']}"
+      
+  await client.send_chat_action(message.chat.id, "typing")
+  await message.reply_text(idz)
 
 
 PM_START_TEXT = """
